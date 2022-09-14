62d87cf23cc006f961a85ab6c520dba163ff84ea
Fabio Aurora <fabioauroradev@gmail.com>
2022-09-14 23:49:13 +0100

    added champion and opponent dialogues

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -4,31 +4,31 @@
 const userName = document.querySelector("#username");
 const createName = document.querySelector("#btn-create");
 const btnRandom = document.querySelector('.btn-random');
-const kingName = document.querySelector("#kingName");
-const kingText = document.querySelector('#king-text');
-const invaderText = document.querySelector('#invader-text');
+const championName = document.querySelector("#champion-name");
+const championDialogue = document.querySelector('#champion-dialogue');
+const opponentDialogue = document.querySelector('#opponent-dialogue');
 const header = document.querySelector('.header');
 
 const showIcons = document.querySelectorAll('figure');
 const battleBTN = document.querySelector('#start-battle');
 const battleCTN = document.querySelector('.battle-ctn');
 
-const selectHeroAvatar = document.querySelectorAll('#hero-avatar');
+const selectChampionAvatar = document.querySelectorAll('#champion-avatar');
 const heroIMG = document.querySelector('#hero-img');
 
 const selectOpponent = document.querySelectorAll('#barbarian-avatar');
 const barbarianIMG = document.querySelector('#barbarian-img');
 
 const barbarianName = document.querySelector('#barbarian-name');
-const OpponentName = document.querySelector('#opponent-name');
-const knightName = document.querySelector('#knight-name');
+const OpponentAction = document.querySelector('#opponent-action');
+const championAction = document.querySelector('#champion-action');
 
 const removeName = document.querySelector('#remove-name');
 
 /* *********************************************************************************** */
 // listener for the create name button
 createName.addEventListener("click", () => {
-  kingName.textContent = userName.value;
+  championName.textContent = userName.value;
 });
 
 /* ************************************************************************************ */
@@ -76,7 +76,7 @@ function randomName() {
   ];
 
   const selectIndex = names[Math.floor(Math.random() * names.length)];
-  return kingName.textContent = selectIndex;
+  return championName.textContent = selectIndex;
 }
 
 btnRandom.addEventListener('click', randomName);
@@ -99,18 +99,15 @@ function displayToggle() {
 
 //king name validation and starting the battle
 function startBattle() {
-  if(kingName.textContent === 'Knight') {
-    knightName.textContent = `\u2014 "Kindly ask's you."`;
-    kingText.innerHTML = `Please give me a epic and glorious  name, so the world knows my story after this battle, against the <strong>${opponentClass.textContent}</strong>, <strong>${barbarianName.textContent}</strong>.`;
-    OpponentName.textContent = `\u2014 "Loudly asked"`
-    invaderText.textContent = `What's your name Knight?, I need to know the name of the one I'm about to defeat.`;
-  } else if(kingName.textContent === '') {
-    kingText.textContent = "I'm sure my parents gave me a name..., but I have Amnesia, can you please help me by creating one or just press Random";
-    invaderText.textContent = ' HA HA HA HA, this will be fun!!!'
+  if(championName.textContent === 'Knight') {
+    championAction.textContent = `\u2014 "Kindly ask's you."`;
+    championDialogue.innerHTML = `Please give me a epic and glorious  name, so the world knows my story after this battle, against the <strong>${opponentClass.textContent}</strong>, <strong>${barbarianName.textContent}</strong>.`;
+    OpponentAction.textContent = `\u2014 "Loudly asked"`
+    opponentDialogue.textContent = `What's your name Knight?, I need to know the name of the one I'm about to defeat.`;
+  } else if(championName.textContent === '') {
+    championDialogue.textContent = "I'm sure my parents gave me a name..., but I have Amnesia, can you please help me by creating one or just press Random";
+    opponentDialogue.textContent = ' HA HA HA HA, this will be fun!!!'
   }else {
-    knightName.textContent = `\u2014 "Challenge's ${barbarianName.textContent}"`;
-    kingText.textContent = `I'm king ${kingName.textContent}, How dare you to invade my Kingdom??, Prepare for BATTLE!!!!!`;
-    invaderText.textContent = `I'm here to defeat you ${kingName.textContent}, your kingdom will be mine, ATTACK!!!!`;
     displayToggle();
   }
 }
@@ -121,25 +118,33 @@ battleBTN.addEventListener('click', startBattle);
 
 // Select Warrior
 const knightClass = document.querySelector('#knight-class');
-  selectHeroAvatar.forEach(king => king.addEventListener('change', choseAvatar));
+selectChampionAvatar.forEach(king => king.addEventListener('change', choseAvatar));
 
   function choseAvatar() {
     switch(this.value) {
-      case 'king':
+      case 'grand officer':
         heroIMG.setAttribute('src', 'assets/images/king.jpg');
         knightClass.textContent = `Grand Officer`;
+        championAction.textContent = `\u2014 "Says with low voice"`;
+        championDialogue.textContent = `Most have been forgotten. Most deserve to be forgotten. The heroes will always be remembered. The best. The best and the worst. And a few who were a bit of both.`;
         break;
-      case 'king1':
+      case 'holy knight 1':
         heroIMG.setAttribute('src', 'assets/images/king1.jpg');
         knightClass.textContent = `Holy Knight`;
+        championAction.textContent = `\u2014 "Prays on he's knees."`;
+        championDialogue.textContent = `War seems like a fine adventure, the greatest most of them will ever know. Then they get a taste of battle.`;
         break;
-      case 'king3':
+      case 'holy knight 2':
         heroIMG.setAttribute('src', 'assets/images/king3.jpg');
         knightClass.textContent = `Holy Knight`;
+        championAction.textContent = `\u2014 "Prays on he's knees."`;
+        championDialogue.textContent = `I will fight until I die, but I will protect you`;
         break;
-      case 'king4':
+      case 'templar':
         heroIMG.setAttribute('src', 'assets/images/king4.jpg');
         knightClass.textContent = `Templar`;
+        championAction.textContent = `\u2014 "Approaches opponent"`;
+        championDialogue.textContent = `In times of war, as in life, surround yourself with people of value, virtue and high morals, because it's always better to lose, perish and vanish in glory than to live in shame.`;
         break;
       case 'king5':
         heroIMG.setAttribute('src', 'assets/images/king5.jpg');
@@ -148,50 +153,74 @@ const knightClass = document.querySelector('#knight-class');
       case 'king6':
         heroIMG.setAttribute('src', 'assets/images/king6.png');
         knightClass.textContent = `White Knight`;
+        championAction.textContent = `\u2014 "Chants"`;
+        championDialogue.textContent = `Hail Holy Queen of mercy. Our life, our consolidation and our hope, hail!. To thee we cry, the banished sons of Eve. To thee we sigh groaning and weeping in this valley of tears.`;
         break;
       case 'king7':
         heroIMG.setAttribute('src', 'assets/images/king7.jpg');
         knightClass.textContent = `Teutonic Knight`;
+        championAction.textContent = `\u2014 "To be added..."`;
+        championDialogue.textContent = `To be added....`;
         break;
       case 'king8':
         heroIMG.setAttribute('src', 'assets/images/king8.jpg');
         knightClass.textContent = `Companion`;
+        championAction.textContent = `\u2014 "To be added..."`;
+        championDialogue.textContent = `To be added....`;
         break;
       case 'king9':
         heroIMG.setAttribute('src', 'assets/images/king9.jpg');
         knightClass.textContent = `Paladin`;
+        championAction.textContent = `\u2014 "To be added..."`;
+        championDialogue.textContent = `To be added....`;
         break;
       case 'king10':
         heroIMG.setAttribute('src', 'assets/images/king10.jpg');
         knightClass.textContent = `Paladin Guardian`;
+        championAction.textContent = `\u2014 "To be added..."`;
+        championDialogue.textContent = `To be added....`;
         break;
       case 'king11':
         heroIMG.setAttribute('src', 'assets/images/king11.jpg');
         knightClass.textContent = `Elven Knight`;
+        championAction.textContent = `\u2014 "To be added..."`;
+        championDialogue.textContent = `To be added....`;
         break;
       case 'king12':
         heroIMG.setAttribute('src', 'assets/images/king12.jpg');
         knightClass.textContent = `Dragon Hunter`;
+        championAction.textContent = `\u2014 "To be added..."`;
+        championDialogue.textContent = `To be added....`;
         break;
         case 'king13':
           heroIMG.setAttribute('src', 'assets/images/king13.jpg');
           knightClass.textContent = `Dragon Hunter`;
+          championAction.textContent = `\u2014 "To be added..."`;
+          championDialogue.textContent = `To be added....`;
           break;
         case 'king14':
           heroIMG.setAttribute('src', 'assets/images/king14.jpg');
           knightClass.textContent = `Defender`;
+          championAction.textContent = `\u2014 "To be added..."`;
+          championDialogue.textContent = `To be added....`;
           break;
         case 'king15':
           heroIMG.setAttribute('src', 'assets/images/king15.jpg');
           knightClass.textContent = `Dragon Hunter`;
+          championAction.textContent = `\u2014 "To be added..."`;
+          championDialogue.textContent = `To be added....`;
           break;
         case 'king16':
           heroIMG.setAttribute('src', 'assets/images/king16.jpg');
           knightClass.textContent = `Grand Commander`;
+          championAction.textContent = `\u2014 "To be added..."`;
+          championDialogue.textContent = `To be added....`;
           break;
         case 'king17':
           heroIMG.setAttribute('src', 'assets/images/king17.jpg');
           knightClass.textContent = `Northen Knight`;
+          championAction.textContent = `\u2014 "To be added..."`;
+          championDialogue.textContent = `To be added....`;
           break;
     }
   }
@@ -209,46 +238,64 @@ function choseBarbarian() {
       barbarianIMG.setAttribute('src', 'assets/images/barbarian.png');
       barbarianName.textContent = `Thralr`;
       opponentClass.textContent = `Barbarian Legend`;
+      OpponentAction.textContent = `\u2014 "Says excited"`;
+      opponentDialogue.textContent = `HA HA!, every warrior wants a worthy opponent. There is no redress in fighting the pathetic.`;
       break;
     case 'barbarian1':
       barbarianIMG.setAttribute('src', 'assets/images/barbarian1.jpg');
       barbarianName.textContent = `Harald Bloodaxe`;
       opponentClass.textContent = `Viking  úlfheðnar`;
+      OpponentAction.textContent = `\u2014 "Battlecry"`;
+      opponentDialogue.textContent = `Better it is to die in battle with honor, than to live in shame because you did not defend your people.`;
       break;
     case 'barbarian2':
       barbarianIMG.setAttribute('src', 'assets/images/barbarian2.jpg');
       barbarianName.textContent = `Gunnstein`;
       opponentClass.textContent = `Viking Berserker`;
+      OpponentAction.textContent = `\u2014 "Battlecry"`;
+      opponentDialogue.textContent = `Run from me… and I will tear apart the mountains to find you! I will follow you to the end of the world!`;
       break;
     case 'barbarian3':
       barbarianIMG.setAttribute('src', 'assets/images/barbarian3.jpg');
       barbarianName.textContent = `Arnbjörg`;
       opponentClass.textContent = `Barbarian King`;
+      OpponentAction.textContent = `\u2014 "Angry voice"`;
+      opponentDialogue.textContent = `You speak of justice? or cowardice?, I will show you the justice of the grave, and the true meaning of fear.`;
       break;
     case 'barbarian4':
       barbarianIMG.setAttribute('src', 'assets/images/barbarian4.jpg');
       barbarianName.textContent = `Bóthildr`;
       opponentClass.textContent = `Barbarian Legend`;
+      OpponentAction.textContent = `\u2014 "To be added..."`;
+      opponentDialogue.textContent = `To be added...`;
       break;
     case 'barbarian5':
       barbarianIMG.setAttribute('src', 'assets/images/barbarian5.jpg');
       barbarianName.textContent = 'Peghed';
       opponentClass.textContent = `Shaman`;
+      OpponentAction.textContent = `\u2014 "To be added..."`;
+      opponentDialogue.textContent = `To be added...`;
       break;
     case 'barbarian6':
       barbarianIMG.setAttribute('src', 'assets/images/barbarian6.jpg');
       barbarianName.textContent = `Sowar`;
       opponentClass.textContent = `Mage`;
+      OpponentAction.textContent = `\u2014 "To be added..."`;
+      opponentDialogue.textContent = `To be added...`;
       break;
     case 'barbarian7':
       barbarianIMG.setAttribute('src', 'assets/images/barbarian7.jpg');
       barbarianName.textContent = `Eyvindur`;
       opponentClass.textContent = `Dark Knight`;
+      OpponentAction.textContent = `\u2014 "To be added..."`;
+      opponentDialogue.textContent = `To be added...`;
       break;
     case 'barbarian8':
       barbarianIMG.setAttribute('src', 'assets/images/barbarian8.jpg');
       barbarianName.textContent = `Ùisdean the Devourer`;
       opponentClass.textContent = `Dark Lord`;
+      OpponentAction.textContent = `\u2014 "To be added..."`;
+      opponentDialogue.textContent = `To be added...`;
       break;
   }
 }
@@ -332,17 +379,32 @@ function playRound(playerSelection, computerSelection) {
     case 'shield':
       if(computerSelection === 'dagger' || computerSelection === 'blade-spin' || computerSelection === 'shuriken') {
         computerLoseRound();
-        OpponentName.textContent = `\u2014 "used ${computerSelection} "`
-        invaderText.textContent = `You keep defending my Attacks, but I will destroy that shield soon.`;
-        knightName.textContent = `\u2014 "Used ${playerSelection}"`;
-        kingText.textContent = `Used my Shield just in time to defend that`;
+        OpponentAction.textContent = `\u2014 "used ${computerSelection} "`
+        opponentDialogue.textContent = `I will destroy that shield soon.`;
+        championAction.textContent = `\u2014 "Blocked with ${playerSelection}"`;
+        championDialogue.textContent = `You know I can block that attack.`;
       }else if(computerSelection === 'heavy-strike') {
         brokenShield();
-        displayRoundMessage(`ohhh no my shield...`)
+        OpponentAction.textContent = `\u2014 "used ${computerSelection} "`
+        opponentDialogue.textContent = `You have no shield now to hide... ATTACK!!`;
+        championAction.textContent = `\u2014 " Tried to Block with ${playerSelection}"`;
+        championDialogue.textContent = `That attack was to powerful for my shield`;
       }else if(computerSelection === 'golden-armor') {
-        displayRoundMessage(noEffect);
+        OpponentAction.textContent = `\u2014 "used Berserker Aura "`
+        opponentDialogue.textContent = `Beeerrzerrrkkeerrr AURAAAAA, your attacks can't do nothing this round.`;
+        championAction.textContent = `\u2014 "Used ${playerSelection}"`;
+        championDialogue.textContent = `That Battle cry was impressive!`;
       }else if(computerSelection === 'health-potion') {
-        displayRoundMessage(noEffect);
+        OpponentAction.textContent = `\u2014 "${computerSelection}"`
+        opponentDialogue.textContent = `preparing herbal Elixir to increase health.`;
+        championAction.textContent = `\u2014 "Used ${playerSelection}"`;
+        championDialogue.textContent = `Defensive Stance with shield`;
+      }else if(playerSelection === computerSelection) {
+        OpponentAction.textContent = `\u2014 "${computerSelection}"`
+        opponentDialogue.textContent = `Defensive Stance with shield`;
+        championAction.textContent = `\u2014 "Used ${playerSelection}"`;
+        championDialogue.textContent = `Defensive Stance with shield`;
+        displayRoundMessage(tie);
       }else {
         playerLoseRound();
         displayRoundMessage(opponentMessage);
@@ -433,7 +495,7 @@ useSkills.forEach(icon => icon.addEventListener('click', () => {
 }))
 
 function displayRoundMessage(message) {
-  kingText.textContent = message;
+  championDialogue.textContent = message;
 }
 
 const addPlayerHearts = document.querySelector('#champion-health-bar');
